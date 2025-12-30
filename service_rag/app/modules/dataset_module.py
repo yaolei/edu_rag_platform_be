5f350a11cef82d4ab7c6ef38d980f0f67e970de7
Evan Yao <yao_lei_java@hotmail.com>
2025-12-30 14:20:33 +0800

    add new colum doc type for db

--- service_rag/app/modules/dataset_module.py
+++ service_rag/app/modules/dataset_module.py
@@ -10,6 +10,7 @@ class KnowledgeItemORM(Base):
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     knowledgeName = Column(String(94), nullable=False)
     activate = Column(Boolean, default=False)
+    doc_type= Column(String(24), default='document')
     deleted = Column(Boolean, default=False, server_default=false(), nullable=False)
     created_at = Column(DateTime, default=datetime.now)
     corpus_id = Column(TEXT)
